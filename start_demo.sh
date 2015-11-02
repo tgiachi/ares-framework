b78ae37d14dae1cbde6151af802079efb14e993d
Tommaso Giachi <squid@stormwind.it>
2015-11-02 17:41:51 +0100

    Aggiornato per la demo

--- start_demo.sh
+++ start_demo.sh
@@ -16,6 +16,12 @@ function copy_files
   cp ./default/* -Rf $ARES_DEMO_DIR
 }
 
+function gradle_wrap_installer
+{
+  sudo curl -o /usr/local/bin/gradle-wrapper-here -L https://github.com/rholder/gradle-wrapper-here/releases/download/v0.1.3/gradle-wrapper-here && \
+  sudo chmod +x /usr/local/bin/gradle-wrapper-here
+  gradle-wrapper-here
+}
 
 function main
 {
@@ -23,6 +29,7 @@ echo "-- Partenza demo per Ares framework --"
 
 create_dirs
 copy_files
+gradle_wrap_installer
 ./gradlew appRun
 }
 
